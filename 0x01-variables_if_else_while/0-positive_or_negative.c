179bb66f6a163465dc0f7b0e898676de44fc2b72
Hazem281 <hazemkamal281@gmail.com.com>
2023-10-19 06:45:26 -0700

    SDS

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <sdio.h>
+/**
+ * Main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return:0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	printif("%i is positive\n");
+	else if (n == 0)
+	printif("%i is positive\n");
+	else
+	printif("%i is negative\n");
+
+	return (0);
+}
